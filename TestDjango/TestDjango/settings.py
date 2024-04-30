192e414acfa2478cc1ba8a823b513631b4ab6117
Lisette <li.droguett@duocuc.cl>
2024-04-29 21:34:23 -0400

    Cambio user y pass

--- TestDjango/TestDjango/settings.py
+++ TestDjango/TestDjango/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.oracle',
          'NAME': 'xe',
-         'USER': 'Wiki_Juego',
-         'PASSWORD': 'Wiki_S5',
+         'USER': 'TIENDA4',
+         'PASSWORD': 'Tienda1234',
          'HOST': 'localhost',
          'PORT': '1521',
          'TEST': {
